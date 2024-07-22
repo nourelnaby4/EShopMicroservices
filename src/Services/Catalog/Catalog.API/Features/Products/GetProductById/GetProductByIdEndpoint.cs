444fc5e6a5449e3955b852c2fcbe22bd6c9d07f7
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-07-23 01:31:57 +0300

    feate(catalog): set endpoint get product by id

---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Features.Products.GetProducts;
+
+namespace Catalog.API.Features.Products.GetProductById;
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("product/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+           .WithName("GetByIdProduct")
+          .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status400BadRequest)
+          .WithSummary("Get By Id Product")
+          .WithDescription("Get By Id Product");
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Features.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdHandler
+    (IDocumentSession session, ILogger<GetProductByIdHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await session.LoadAsync<Product>(query.Id,cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
