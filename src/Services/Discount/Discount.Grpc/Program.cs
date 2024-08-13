1cbc761c4e7be6aac9c4cbffaa3defbc94ccac6d
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-08-13 20:21:47 +0300

    [fix/discount] fix protobuf model

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,3 @@
-using Discount.Grpc.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +6,6 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
