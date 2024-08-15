b2db29de3088e058fa2d8e7e5b3d01b1d372cdbf
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-08-15 23:31:41 +0300

    [fix] handle allow grpc request incase ssl certificated is missed

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -27,8 +27,17 @@ builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
-});
+})
+  .ConfigurePrimaryHttpMessageHandler(() =>
+  {
+      var handler = new HttpClientHandler
+      {
+          ServerCertificateCustomValidationCallback =
+          HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+      };
 
+      return handler;
+  });
 
 // Data services
 builder.Services.AddMarten(options =>
