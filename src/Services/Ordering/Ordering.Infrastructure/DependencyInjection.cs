246bd5c5519db6b510ce4f8f29e4fe6f1163311f
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-08-20 20:13:13 +0300

    [feate/ordering] Create EF Core Migrations for ApplicationDbContext in Ordering.Infrastructure

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -2,11 +2,15 @@
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure;
-
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
         return services;
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Infrastructure.Data;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
