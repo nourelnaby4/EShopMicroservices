557d5207625d41d41ae0357d309c938caef10541
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-08-22 00:24:25 +0300

    [feate/order] Develop Get Orders Using Pagination

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -1,4 +1,4 @@
-﻿namespace Ordering.Application.Orders.Queries.GetOrderByCustomer;
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
 
 public record GetOrdersByCustomerQuery(Guid CustomerId)
     : IQuery<GetOrdersByCustomerResult>;
