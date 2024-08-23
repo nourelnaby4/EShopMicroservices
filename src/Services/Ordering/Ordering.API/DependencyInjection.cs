e35bb6296f36d0df6bb40330db9da74169464d45
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-08-23 03:48:31 +0300

    [feate/order] add health check

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,16 +1,35 @@
-﻿namespace Ordering.API;
+﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services,IConfiguration configuration)
     {
         services.AddCarter();
+
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
+
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app)
     {
         app.MapCarter();
+
+        app.UseExceptionHandler(options => { });
+
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
         return app;
     }
 }
