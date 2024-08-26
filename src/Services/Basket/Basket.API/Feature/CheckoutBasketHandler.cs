18dcdb74dc44867dda1e3b3faba6cb66abcb991e
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-08-26 19:12:46 +0300

    [Feate] Develop Basket Checkout Handler Method to Publish event to RabbitMQ-MassTransit

---
+++ src/Services/Basket/Basket.API/Feature/CheckoutBasketHandler.cs
@@ -0,0 +1,41 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Feature;
+
+public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckoutDto)
+    : ICommand<CheckoutBasketResult>;
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValidator
+    : AbstractValidator<CheckoutBasketCommand>
+{
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("BasketCheckoutDto can't be null");
+        RuleFor(x => x.BasketCheckoutDto.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class CheckoutBasketCommandHandler
+    (IBasketRepository repository, IPublishEndpoint publishEndpoint)
+    : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        var basket = await repository.GetBasket(command.BasketCheckoutDto.UserName, cancellationToken);
+        if (basket == null)
+        {
+            return new CheckoutBasketResult(false);
+        }
+
+        var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+        await repository.DeleteBasket(command.BasketCheckoutDto.UserName, cancellationToken);
+
+        return new CheckoutBasketResult(true);
+    }
+}
--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -7,4 +7,5 @@ global using BuildingBlocks.CQRS;
 global using Basket.API.Models;
 global using Basket.API.Exceptions;
 global using Basket.API.Data;
+global using Basket.API.Dtos;
 
